d18e96552452144a6bb1abca998f05c2bdf52c86
Paula Alonso <mjsafr951753@gmail.com>
2026-03-16 10:25:05 +0100

    javadoc helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+// Ejemplo de hola mundo
+
+public class HelloWorld {
+	/** @return devuelve hola mundo
+	 * @param String[]args
+	 */
+	public static void main(String[]args){
+	 System.out.println("!Hola,Mundo!");
+}
+
+
+}
